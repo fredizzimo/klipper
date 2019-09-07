62c139bcdcd62526f6dc4eda05102db6f4d64faa
Fred Sundvik <fsundvik@gmail.com>
2019-09-07 21:58:45 +0300

    Don't use out of date chelper library
    Delete the old library before compiling to make sure that it's not used
    in case the compilation fails.
    
    Signed-off-by: Fred Sundvik <fsundvik@gmail.com>

--- klippy/chelper/__init__.py
+++ klippy/chelper/__init__.py
@@ -159,6 +159,9 @@ def check_build_code(srcdir, target, sources, cmd, other_files=[]):
         logging.info("Building C code module %s", target)
         srcfiles = [os.path.join(srcdir, fname) for fname in sources]
         destlib = os.path.join(srcdir, target)
+        # Make sure we don't use an out of date library
+        if os.path.isfile(destlib):
+            os.remove(destlib)
         res = os.system(cmd % (destlib, ' '.join(srcfiles)))
         if res:
             msg = "Unable to build C code module (error=%s)" % (res,)
