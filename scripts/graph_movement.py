b3c384b86c12a2badd675a7f73fd1d9a05e6ae13
Fred Sundvik <fsundvik@gmail.com>
2020-04-08 19:44:56 +0300

    Correctly calculate the movement times

--- scripts/graph_movement.py
+++ scripts/graph_movement.py
@@ -11,20 +11,25 @@ import errno
 import numpy as np
 import plotly.graph_objects as go
 import plotly.io as pio
+from plotly.subplots import make_subplots
 
 from os import path
 sys.path.append(path.normpath(
     path.join(path.split(__file__)[0] + "/..")))
 from klippy.msgproto import MessageParser
 
+MASK_32_BIT = 0xFFFFFFFF
+MASK_32_BIT_HIGH = MASK_32_BIT << 32
+
 class Stepper(object):
-    def __init__(self, message):
+    def __init__(self, message, clock_freq):
         self.oid = message["oid"]
         self.invert_step = message["invert_step"]
         self.step_clock = 0
         self.dir = not self.invert_step
         self.steps = [(0, 0)]
         self.pos = 0
+        self.freq = clock_freq
     def reset_step_clock(self, message):
         self.step_clock = message["clock"]
     def set_next_step_dir(self, message):
@@ -33,26 +38,45 @@ class Stepper(object):
         interval = message["interval"]
         count = message["count"]
         add = message["add"]
-        t = self.step_clock
+        base_clock = self.get_message_clock(message)
+        base_low = base_clock & MASK_32_BIT
+        base_high = base_clock & MASK_32_BIT_HIGH
+
+        wrapped_first = self.step_clock + interval
+        wrapped_first &= MASK_32_BIT
+        if (wrapped_first < base_low):
+            base_high += 0x100000000
+        t = base_high + wrapped_first - interval
         pos = self.pos
         step = -1 if self.dir else 1
         for _ in range(count):
             t += interval
+            self.steps.append((t / self.freq, pos))
             pos += step
-            self.steps.append((t, pos))
             interval += add
         self.pos = pos
-        self.step_clock = t
-    def calculate_moves(self):
+        self.step_clock = t & MASK_32_BIT
+    def calculate_moves(self, start_time):
         self.steps = np.array(self.steps)
+        self.steps[0][0] = start_time
+        self.steps[:,0] -= start_time
+        pass
+    def get_message_clock(self, message):
+        return int(message["timestamp"]*self.freq)
+
 
 def graph_moves(steppers, output_path):
     steppers = sorted(list(steppers.values()), key=lambda x: x.oid) 
     fig = go.Figure()
-    for stepper in steppers:
+    fig = make_subplots(rows=len(steppers), cols=1, 
+                    shared_xaxes=True, 
+                    vertical_spacing=0.02)
+    for i, stepper in enumerate(steppers):
         fig.add_trace(go.Scatter(
             x=stepper.steps[:,0], y=stepper.steps[:,1], name="%i" % stepper.oid,
-            line=go.scatter.Line()))
+            line=go.scatter.Line()),
+            row=i+1, col=1,
+            )
     
     filename = path.join(output_path, "steppers.html")
     pio.write_html(fig, filename, include_plotlyjs=True, full_html=True)
@@ -83,13 +107,14 @@ def main():
     message_parser = MessageParser()
     message_parser.process_identify(dictionary, decompress=False)
     messages = message_parser.parse_file(args.input)
+    clock_freq = message_parser.get_constant_float('CLOCK_FREQ')
     steppers = {}
     for m in messages:
         name = m["name"]
         if name == "queue_step":
             steppers[m["oid"]].queue_step(m)
         elif name == "config_stepper":
-            stepper = Stepper(m)
+            stepper = Stepper(m, clock_freq)
             steppers[stepper.oid] = stepper
         elif name == "finalize_config":
             print("Num steppers %i" % len(steppers))
@@ -98,8 +123,10 @@ def main():
         elif name == "set_next_step_dir":
             steppers[m["oid"]].set_next_step_dir(m)
     
+    start_time = messages[0]["timestamp"]
+    
     for s in steppers.itervalues():
-        s.calculate_moves()
+        s.calculate_moves(start_time)
 
     create_output_directory(args.output)
     graph_moves(steppers, args.output)
