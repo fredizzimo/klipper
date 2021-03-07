8f8a75f84c97b9d647eab32f089abe7a7c502e0a
Fred Sundvik <fsundvik@gmail.com>
2021-03-08 00:24:16 +0200

    Fix the deceleration calculation
    Add reference velocity

--- notebooks/smooth_decelerate.py
+++ notebooks/smooth_decelerate.py
@@ -15,7 +15,7 @@ display(eq_delta_x)
 # %%
 delta_step0 = sy.Symbol("Delta_step0")
 t_0 = sy.Symbol("t_0")
-eq_delta_step0 = sy.Eq(delta_step0, v*t_0 + a/2*t_0**2)
+eq_delta_step0 = sy.Eq(delta_step0, v*t_0 - a/2*t_0**2)
 display(eq_delta_step0)
 eq_t_step0 = sy.Eq(t_0, sy.solve(eq_delta_step0, t_0)[0])
 display(eq_t_step0)
@@ -36,16 +36,19 @@ def generate_intervals(v, a, step_dist):
     a = float(a)
     num_steps = (v**2) / (2*a)
     num_steps /= step_dist
-    #num_steps = int(floor(num_steps))
-    t_step_0 = (-v + sqrt(2*step_dist*a + v**2)) / a
-    
-    c = t_step_0
-    intervals = []
+    if num_steps >= 1.0:
+        #num_steps = int(floor(num_steps))
+        t_step_0 = (v - sqrt(-2*step_dist*a + v**2)) / a
+        
+        c = t_step_0
+        intervals = [t_step_0]
 
-    for n in range(0, int(floor(num_steps))):
-        c = c - (2.0 * c) / (4.0*(n - num_steps) + 1)
-        intervals.append(c)
-    return intervals
+        for n in range(1, int(floor(num_steps))):
+            c = c - (2.0 * c) / (4.0*(n - num_steps) + 1)
+            intervals.append(c)
+        return intervals
+    else:
+        return []
     
 def generate_t(intervals):
     ts = []
@@ -58,20 +61,25 @@ def generate_t(intervals):
 def generate_x(ts, step_dist):
     return [(i+1)*step_dist for i in range(len(ts))]
 
-def generate_v(intervals, step_dist):
-    vs = []
-    return [step_dist / interval for interval in intervals]     
+def generate_v(intervals, ts, step_dist):
+    shifted_left = [0] + ts
+    ts = [prev + (next-prev) / 2.0 for prev, next in zip(shifted_left[:-1], ts)]
+    vs = [step_dist / interval for interval in intervals]
+    return vs, ts
+
 
 # %%
 def plot_segments(v, a, step_dist):
     intervals = generate_intervals(v, a, step_dist)
-    t = generate_t(intervals)
-    x = generate_x(t, step_dist)
-    v = generate_v(intervals, step_dist)
+    ts = generate_t(intervals)
+    xs = generate_x(ts, step_dist)
+    vs, vts = generate_v(intervals, ts, step_dist)
+    v2s = [v - a*t for t in ts] 
     fig = make_subplots(specs=[[{"secondary_y": True}]])
-    fig.add_trace(go.Scatter(x=t, y=x, name="x"), secondary_y=False)
-    fig.add_trace(go.Scatter(x=t, y=v, name="v"), secondary_y=True)
+    fig.add_trace(go.Scatter(x=ts, y=xs, name="x"), secondary_y=False)
+    fig.add_trace(go.Scatter(x=vts, y=vs, name="v"), secondary_y=True)
+    fig.add_trace(go.Scatter(x=ts, y=v2s, name="vref"), secondary_y=True)
     fig.show()
 
-plot_segments(5, 300, 0.0025)
+plot_segments(10, 300, 0.0025)
 # %%
