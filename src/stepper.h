dc09619fbaa17ebec0b7920298ca73cc90ea1a7d
Fred Sundvik <fsundvik@gmail.com>
2021-03-09 01:31:34 +0200

    Disable smooth stop on pru
    It does not have enough code memory to allow it

--- src/stepper.h
+++ src/stepper.h
@@ -6,6 +6,10 @@
 uint_fast8_t stepper_event(struct timer *t);
 struct stepper *stepper_oid_lookup(uint8_t oid);
 void stepper_stop(struct stepper *s);
+#if CONFIG_HAVE_SMOOTH_STOP
 void stepper_smooth_stop(struct stepper *s);
+#else
+#define stepper_smooth_stop stepper_stop
+#endif
 
 #endif // stepper.h
