32af2002fed641853c80eccde9a4ba094438df0f
Fred Sundvik <fsundvik@gmail.com>
2021-03-08 02:40:59 +0200

    Implement smooth decelerate closer to real implementation

--- notebooks/smooth_decelerate.py
+++ notebooks/smooth_decelerate.py
@@ -31,24 +31,48 @@ display(eq_c_n)
 display(eq_c_0)
 
 # %%
-def generate_intervals(v, a, step_dist):
-    v = float(v)
-    a = float(a)
-    num_steps = (v**2) / (2*a)
-    num_steps /= step_dist
-    if num_steps >= 1.0:
-        #num_steps = int(floor(num_steps))
-        t_step_0 = (v - sqrt(-2*step_dist*a + v**2)) / a
-        
-        c = t_step_0
-        intervals = [t_step_0]
+def generate_intervals(v, a, step_dist, freq):
+    #emulate what we have on the firmware side
+    interval = 1.0 / v
+    interval *= step_dist
+    interval *= freq
+    interval = int(round(interval))
 
-        for n in range(1, int(floor(num_steps))):
-            c = c - (2.0 * c) / (4.0*(n - num_steps) + 1)
+    # a is passed as a/step_dist
+    a = int(round(float(a)/step_dist))
+    
+    # these can be precalculated when configuring
+    a_inv = 1.0 / float(a)
+    two_a = 2*a
+    two_a_inv = 1.0 / two_a
+    
+    # actual code that needs to be implemented starts here
+    
+    v = float(freq) / interval
+    v2 = v*v
+
+    radicand = v2 - two_a
+
+    if radicand > 0:
+        t_step_0 = (v - sqrt(radicand)) * a_inv
+
+        # Use integer math, so that everything is deterministic
+        num_steps = int(round(v2 * two_a_inv))
+        
+        c = int(round(t_step_0 * freq))
+        intervals = [c]
+
+        divisor = 4 - 4 * num_steps + 1
+        for n in range(1, num_steps-1):
+            c = c - (2 * c) / divisor
+            divisor += 4
             intervals.append(c)
         return intervals
     else:
         return []
+
+def convert_intervals(intervals, freq):
+    return [float(t) / freq for t in intervals]
     
 def generate_t(intervals):
     ts = []
@@ -69,8 +93,9 @@ def generate_v(intervals, ts, step_dist):
 
 
 # %%
-def plot_segments(v, a, step_dist):
-    intervals = generate_intervals(v, a, step_dist)
+def plot_segments(v, a, step_dist, freq):
+    intervals = generate_intervals(v, a, step_dist, freq)
+    intervals = convert_intervals(intervals, freq)
     ts = generate_t(intervals)
     xs = generate_x(ts, step_dist)
     vs, vts = generate_v(intervals, ts, step_dist)
@@ -81,5 +106,5 @@ def plot_segments(v, a, step_dist):
     fig.add_trace(go.Scatter(x=ts, y=v2s, name="vref"), secondary_y=True)
     fig.show()
 
-plot_segments(10, 300, 0.0025)
+plot_segments(5, 300, 0.0025, 72000000)
 # %%
