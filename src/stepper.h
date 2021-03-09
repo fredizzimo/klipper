64792034c1f56bc051b57128c684f44047e3e7e5
Fred Sundvik <fsundvik@gmail.com>
2021-03-09 11:52:07 +0200

    Include autoconfig in stepper.h

--- src/stepper.h
+++ src/stepper.h
@@ -2,6 +2,7 @@
 #define __STEPPER_H
 
 #include <stdint.h> // uint8_t
+#include "autoconf.h" // CONFIG_*
 
 uint_fast8_t stepper_event(struct timer *t);
 struct stepper *stepper_oid_lookup(uint8_t oid);
