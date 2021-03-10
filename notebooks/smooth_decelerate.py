af2703751cc061e23119e84f5bba76407c43c0f9
Fred Sundvik <fsundvik@gmail.com>
2021-03-11 01:10:54 +0200

    Calculations for a ramp that returns to start position

--- notebooks/smooth_decelerate.py
+++ notebooks/smooth_decelerate.py
@@ -64,33 +64,84 @@ def generate_intervals(v, a, step_dist, freq):
         divisor = 4 * num_steps - 5
         
         intervals = []
-        while num_steps > 0:
-            intervals.append(c)
+        num_deceleration1_steps = num_steps
+        # Round up
+        num_acceleration_steps = (num_steps + 1) / 2
+        num_deceleration2_steps = num_steps - num_acceleration_steps
+        print(num_deceleration1_steps)
+        print(num_acceleration_steps)
+        print(num_deceleration2_steps)
+        # The last step will be taken when switching direction
+        while num_deceleration1_steps > 1:
+            print("decelerate 1")
+            intervals.append((c, 1))
+            print(divisor)
             c = c + (2 * c) / divisor
             divisor -= 4
-            num_steps -= 1
+            num_deceleration1_steps -= 1
+        if num_deceleration1_steps == 1:
+            print("change direction")
+            intervals.append((c, 1))
+            intervals.append((c, -1))
+            divisor = 4 + 1
+            print(divisor)
+            c = c - (2 * c) / divisor
+            divisor += 4
+            num_deceleration1_steps -= 1
+        # We have already taken one step, and will take another
+        # when switching direction again
+        while num_acceleration_steps > 2:
+            print("accelerate")
+            intervals.append((c, -1))
+            print(divisor)
+            c = c - (2 * c) / divisor
+            divisor += 4
+            num_acceleration_steps -= 1
+        if num_acceleration_steps == 2:
+            print("change direction 2")
+            intervals.append((c, -1))
+            intervals.append((c, -1))
+            divisor -= 6;
+            print(divisor)
+            c = c + (2 * c) / divisor
+            divisor -= 4
+            num_deceleration1_steps -= 1
+        while num_deceleration2_steps > 1:
+            print("decelerate 2")
+            intervals.append((c, -1))
+            print(divisor)
+            c = c + (2 * c) / divisor
+            divisor -= 4
+            num_deceleration2_steps -= 1
+            
+            
         return intervals
     else:
         return []
 
 def convert_intervals(intervals, freq):
-    return [float(t) / freq for t in intervals]
+    return [(float(t) / freq, d) for t, d in intervals]
     
 def generate_t(intervals):
     ts = []
     t = 0
-    for interval in intervals:
+    for interval, _ in intervals:
         t += interval
         ts.append(t)
     return ts
 
-def generate_x(ts, step_dist):
-    return [(i+1)*step_dist for i in range(len(ts))]
+def generate_x(intervals, step_dist):
+    dist = 0.0
+    ret = []
+    for _, d in intervals:
+        dist += step_dist * d
+        ret.append(dist)
+    return ret
 
 def generate_v(intervals, ts, step_dist):
     shifted_left = [0] + ts
     ts = [prev + (next-prev) / 2.0 for prev, next in zip(shifted_left[:-1], ts)]
-    vs = [step_dist / interval for interval in intervals]
+    vs = [d*step_dist / interval for (interval, d) in intervals]
     return vs, ts
 
 
@@ -99,7 +150,7 @@ def plot_segments(v, a, step_dist, freq):
     intervals = generate_intervals(v, a, step_dist, freq)
     intervals = convert_intervals(intervals, freq)
     ts = generate_t(intervals)
-    xs = generate_x(ts, step_dist)
+    xs = generate_x(intervals, step_dist)
     vs, vts = generate_v(intervals, ts, step_dist)
     v2s = [v - a*t for t in ts] 
     fig = make_subplots(specs=[[{"secondary_y": True}]])
